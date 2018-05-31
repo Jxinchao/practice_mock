aeeed613981b4a7471e286edfdea6971b51197f5
jxinchao <425775961@qq.com>
2018-05-31 14:20:05 +0800

    ddddd

--- routes/login.js
+++ routes/login.js
@@ -24,29 +24,28 @@ function searchUser(sql,callback){
 
 route.post('/login',function(req,res){
 	res.header("Access-Control-Allow-Origin","*");
-	res.send('ok')
-	// var unames=req.body['username'];
-	// var pwds=req.body['password'];
-	// console.log(unames+'>>>'+pwds)
-	// searchUser('select * from login',function(err,result){
-	// 	console.log(result)
-	// 	for(var user in result){
-	// 		if(unames==result[user].uname&&pwds==result[user].pwd){
-	// 			req.session.username=unames; //设置session
-	// 			//console.log(userId)
-	// 			//res.redirect('html/homepage.html')   // 重定向
-	// 			res.send({flag:1,id:result[user].id})
-	// 			return;
-	// 		}else if(unames==result[user].uname&&pwds!=result[user].pwd){
-	// 			res.send({flag:3})
-	// 			return;
-	// 		}else{
-	// 			if(user==(result.length-1)){
-	// 				res.send({flag:2})
-	// 				return;
-	// 			}
-	// 		}
-	// 	}
+	var unames=req.body['username'];
+	var pwds=req.body['password'];
+	console.log(unames+'>>>'+pwds)
+	searchUser('select * from login',function(err,result){
+		console.log(result)
+		for(var user in result){
+			if(unames==result[user].uname&&pwds==result[user].pwd){
+				req.session.username=unames; //设置session
+				//console.log(userId)
+				//res.redirect('html/homepage.html')   // 重定向
+				res.send({flag:1,id:result[user].id})
+				return;
+			}else if(unames==result[user].uname&&pwds!=result[user].pwd){
+				res.send({flag:3})
+				return;
+			}else{
+				if(user==(result.length-1)){
+					res.send({flag:2})
+					return;
+				}
+			}
+		}
 	// })
 })
 module.exports = route;
