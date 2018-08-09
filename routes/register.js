2bad2ef545b94a14631d7ce4f7dabe50543f9d70
jxinchao <425775961@qq.com>
2018-08-09 16:21:01 +0800

    ...

--- routes/register.js
+++ routes/register.js
@@ -9,31 +9,30 @@ let pool=mysql.createPool({
 		port:3307
 })
 
-router.get('/register',function(requset,response){
+router.post('/register',function(requset,response){
   response.header("Access-Control-Allow-Origin","*");
-  response.send('...')
-  // let uname = requset.body['uname'];
-  // let pwd = requset.body['pwd'];
-  // let tel = requset.body['tel'];
-  // getAllUsers(uname,function(err,results){
-  //     console.log(results);
-  //     if(results != null && results != ''){
-  //         response.send({flag:2});  //用户名被占用
-  //         return;
-  //     }
-  //     save(uname,pwd,tel,function(err,result){
-  //         if(err){
-  //             response.send({flag:3});
-  //             return;
-  //         }
-  //         if(result.insertId > 0){
-  //             response.send({flag:1})  //注册成功
-  //         }else{
-  //             response.send({flag:4})  //注册失败
-  //         }
-  //
-  //     })
-  // })
+  let uname = requset.body['uname'];
+  let pwd 	= requset.body['pwd'];
+  let tel 	= requset.body['tel'];
+  getAllUsers(uname,function(err,results){
+      if(results != null && results != ''){
+          response.send({flag:2});  //用户名被占用
+          return;
+      }
+      save(uname,pwd,tel,function(err,result){
+				console.log(err,uname,pwd,tel,result);
+          if(err){
+              response.send({flag:3});
+              return;
+          }
+          if(result.insertId > 0){
+              response.send({flag:1})  //注册成功
+          }else{
+              response.send({flag:4})  //注册失败
+          }
+
+      })
+  })
 
 });
 //获取信息列表：
@@ -41,31 +40,29 @@ function getAllUsers(uname,call){  //形参以函数的形式传递出去
     pool.getConnection(function(err,connection){  // 成功获取
         var getAllUsers_Sql = 'select * from user where uname=?';
         connection.query(getAllUsers_Sql,[uname],function(err,result){
-            // console.log(result)
             connection.release();  //释放连接数
             call(err,result)
         })
     })
 }
 // 插入数据
-function save(uname,pwd,img,callback){
+function save(uname,tel,pwd,callback){
     pool.getConnection(function(err,connection){  // 成功获取
-        var insert_Sql = 'insert into user(uname,pwd,img) values(?,?,?)'; //id用0站位
-        connection.query(insert_Sql,[uname,pwd,img],function(err,result){
-            // console.log(result)
+        var insert_Sql = 'insert into user(uname,tel,pwd) values(?,?,?)'; //id用0站位
+        connection.query(insert_Sql,[uname,tel,pwd],function(err,result){
             connection.release();  //释放连接数
             callback(err,result)
         })
     })
 }
 // 封装的方法
-function connectionSql(sql,callback){
-  pool.getConnection(function(err,conns){
-    conns.query(sql,function(err,result){
-      conns.release();
-      callback(err,result);
-    })
-  })
-}
+// function connectionSql(sql,callback){
+//   pool.getConnection(function(err,conns){
+//     conns.query(sql,function(err,result){
+//       conns.release();
+//       callback(err,result);
+//     })
+//   })
+// }
 
 module.exports=router;//输出
