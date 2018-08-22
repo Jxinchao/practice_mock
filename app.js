a47cbc7b091a2fc401146b1c1a8e2bd9cf479081
jxinchao <425775961@qq.com>
2018-08-22 10:44:53 +0800

    .

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var logger = require('morgan');
 
 let indexRouter = require('./routes/index');
 let logins = require('./routes/login'); // 登录
-let register = require('./routes/register'); // 注册 
+let register = require('./routes/register'); // 注册
 let append = require('./routes/append');  // 数据
 let usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setup  
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
