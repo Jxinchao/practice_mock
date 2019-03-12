1b1fb5f784545630833b5c33556512b42abdfd83
jxinchao <425775961@qq.com>
2019-03-12 23:06:33 +0800

    ..

--- public/abject/ipsen/js/pub.js
+++ public/abject/ipsen/js/pub.js
@@ -1,14 +1,41 @@
 // 导航
-$(function(){
+$(function() {
 
-  $('.nav li').hover(function(){
+	$('.masthead__main ul li').hover(function() {
+		console.log($(this))
+		$(this).find('.box').stop().show('slow');
 
-    $(this).children('ul').stop(true,true).show('slow');
+	}, function() {
 
-  },function(){
+		$(this).find('.box').stop().hide('slow');
 
-    $(this).children('ul').stop(true,true).hide('slow');
-
-  });
-
+	});
+	
+	
 });
+
+// 吸顶条
+$(function() {
+
+	var titleTop = $('.banner').height();
+	$(document).on('scroll', function() {
+		if($(document).scrollTop() > titleTop) {
+			
+			$('.headers').css({
+				"position": "fixed",
+				"top": 0,
+				"display": "block"
+			})
+			$('.hrt').css({
+				"position": "absolute"
+			})
+		} else {
+			$('.hrt').css({
+				"position": "fixed"
+			})
+			$('.headers').css({
+				"display": "none"
+			})
+		}
+	})
+})
